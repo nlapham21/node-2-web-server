77bc6116316770708c8b28fe084ba30aaf527433
Nolan Lapham <nlapham21@gmail.com>
2018-11-21 11:37:56 -0500

    adding projects page

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about', (req, res) => {
         pageTitle: 'About page'
     });
 });
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'Portfolio page here'
+    })
+});
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
